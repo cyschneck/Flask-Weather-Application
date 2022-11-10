ceb75f565d04350778197db6b000307bee6643e4
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-11-10 14:20:12 -0700

    combine weather api with results.html page

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def get_api_key():
 	return config["openweathermap"]["api_key"]
 
 # take in zip code and api and return the results of api
-def get_weather_request(zip_code, api_key):
+def get_weather_results(zip_code, api_key):
 	# openweathermap.org
-	api_url = "https://api.openweathermap.org/data/2.5/weather?zip={0}&appid={1}".format(zip_code, api_key)
+	api_url = "https://api.openweathermap.org/data/2.5/weather?zip={0}&units=imperial&appid={1}".format(zip_code, api_key)
 	r = requests.get(api_url)
 	return r.json()
 
@@ -27,8 +27,22 @@ def weather_dashboard():
 @app.route("/results", methods=["POST"]) # route for the results page
 def render_results():
 	zip_code = request.form["zipCode"]
-	return "Zip Code: {0}".format(zip_code) # called in app.route("/results")
+	api_key = get_api_key()
+	json_data = get_weather_results(zip_code, api_key)
+
+	# temperature variables
+	temp = "{0:.2f}".format(json_data["main"]["temp"])
+	feels_like_temp = "{0:.2f}".format(json_data["main"]["feels_like"])
+	weather_description = json_data["weather"][0]["main"]
+	location = json_data["name"]
+	
+	# render tempalte for results page with variables set in the html
+	return render_template("results.html",
+						location=location,
+						feels_like=feels_like_temp,
+						weather_description=weather_description,
+						temp=temp)
 
 if __name__ == '__main__':
 	app.run() # only run flask app once
-	print(get_weather_request("80305", get_api_key()))
+	#print(get_weather_results("80305", get_api_key()))
